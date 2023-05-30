4817e203bb8abbd3c8999118f704fce881dab409
weiru <weiru@kanyun.com>
2023-05-30 14:28:05 +0800

    修改base

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app/',
+  base: '/import-map-demo/dist/app/',
   build: {
     minify: false,
     outDir: '../../dist/app',
